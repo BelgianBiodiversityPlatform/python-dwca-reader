9a5d26b76100aa08121652a852270213dbb598a8
Nicolas Noé <nicolas@niconoe.org>
2013-03-22 15:23:45 +0100

    Better example/API/test file.

--- dwca/example.py
+++ dwca/example.py
@@ -1,7 +1,25 @@
+import os
+
 from dwca import DwCAReader
 
-source = DwCAReader('./test/sample_files/dwca-simple-test-archive.zip')
+source_path = os.path.join(os.path.dirname(__file__),
+                           './test/sample_files/dwca-simple-test-archive.zip')
 
-for line in source.each_line():
+# Create the object and open the DwC-A file
+dwca = DwCAReader(source_path)
+
+# Check if a Darwin Core term in present in the core file
+if dwca.core_contains_term('http://rs.tdwg.org/dwc/terms/locality'):
+    print "This archive contains the 'locality' term in its core file."
+else:
+    print "Locality term is not present."
+
+if dwca.core_contains_term('http://rs.tdwg.org/dwc/terms/country'):
+    print "This archive contains the 'country' term in its core file."
+else:
+    print "'Country' term is not present."
+
+# Iterate over each line
+for line in dwca.each_line():
     # line is an instance of DwCALine
     print line
