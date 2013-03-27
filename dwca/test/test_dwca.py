e9ca1a54e965179ad13949e33e56847978221aa3
Nicolas Noé <nicolas@niconoe.org>
2013-03-27 16:44:53 +0100

    PEP8 + refactoring for test file.

--- dwca/test/test_dwca.py
+++ dwca/test/test_dwca.py
@@ -6,35 +6,38 @@ from BeautifulSoup import BeautifulStoneSoup
 from ..dwca import DwCAReader
 from ..dwterms import terms
 
+
+# Helpers
+def _sample_data_path(filename):
+    return os.path.join(os.path.dirname(__file__), 'sample_files', filename)
+
+
 class Test(unittest.TestCase):
     """Unit tests for python-dwca-reader."""
 
-    BASIC_ARCHIVE_PATH = os.path.join(os.path.dirname(__file__),
-                           './sample_files/dwca-simple-test-archive.zip')
-    NOHEADERS1_PATH = os.path.join(os.path.dirname(__file__),
-                           './sample_files/dwca-noheaders-1.zip')
-    NOHEADERS2_PATH = os.path.join(os.path.dirname(__file__),
-                           './sample_files/dwca-noheaders-2.zip')
+    BASIC_ARCHIVE_PATH = _sample_data_path('dwca-simple-test-archive.zip')
+    NOHEADERS1_PATH = _sample_data_path('dwca-noheaders-1.zip')
+    NOHEADERS2_PATH = _sample_data_path('dwca-noheaders-2.zip')
 
     def test_cleanup(self):
         """Test no temporary files are left after execution"""
         num_files_before = len(os.listdir('.'))
 
-        with DwCAReader(self.BASIC_ARCHIVE_PATH) as dwca:
+        with DwCAReader(self.BASIC_ARCHIVE_PATH):
             pass
 
-        num_files_after = len(os.listdir('.'))    
+        num_files_after = len(os.listdir('.'))
 
         self.assertEqual(num_files_before, num_files_after)
 
     def test_temporary_folder(self):
         """Test a temporary folder is created during execution
-        
+
         (complementay to test_cleanup()
         """
 
         num_files_before = len(os.listdir('.'))
-        with DwCAReader(self.BASIC_ARCHIVE_PATH) as dwca:
+        with DwCAReader(self.BASIC_ARCHIVE_PATH):
             num_files_during = len(os.listdir('.'))
 
         self.assertEqual(num_files_before, num_files_during-1)
@@ -44,7 +47,8 @@ class Test(unittest.TestCase):
 
         with DwCAReader(self.BASIC_ARCHIVE_PATH) as dwca:
             # dwca-simple-test-archive.zip should be of Occurrence type
-            self.assertEqual(dwca.core_type, 'http://rs.tdwg.org/dwc/terms/Occurrence')
+            self.assertEqual(dwca.core_type,
+                             'http://rs.tdwg.org/dwc/terms/Occurrence')
             # Check that shortcuts also work
             self.assertEqual(dwca.core_type, terms['OCCURRENCE'])
 
@@ -76,11 +80,13 @@ class Test(unittest.TestCase):
             self.assertEqual(2, len([l for l in dwca.each_line()]))
 
         with DwCAReader(self.NOHEADERS1_PATH) as dwca:
-            # This file has two real lines, without headers (specified in meta.xml)
+            # This file has two real lines, without headers
+            # (specified in meta.xml)
             self.assertEqual(2, len([l for l in dwca.each_line()]))
 
         with DwCAReader(self.NOHEADERS2_PATH) as dwca:
-            # This file has two real lines, without headers (nothing specified in meta.xml)
+            # This file has two real lines, without headers
+            # (nothing specified in meta.xml)
             self.assertEqual(2, len([l for l in dwca.each_line()]))
 
 if __name__ == "__main__":
