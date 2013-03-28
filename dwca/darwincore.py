6b84f1406b66fda76fcd5beb7e3943fc8ae073db
Nicolas Noé <nicolas@niconoe.org>
2013-03-28 15:54:54 +0100

    Refactor qn.

--- dwca/darwincore.py
+++ dwca/darwincore.py
@@ -1,3 +1,13 @@
+TERMS = [
+    'http://rs.tdwg.org/dwc/terms/country',
+    'http://rs.tdwg.org/dwc/terms/decimalLatitude',
+    'http://rs.tdwg.org/dwc/terms/decimalLongitude',
+    'http://rs.tdwg.org/dwc/terms/kingdom',
+    'http://rs.tdwg.org/dwc/terms/locality',
+    'http://rs.tdwg.org/dwc/terms/Occurrence'
+]
+
+
 def qualname(short):
     """Takes a darwin core term and returns the corresponding qualname
 
@@ -5,14 +15,4 @@ def qualname(short):
     raise a StopIteration if none is found.
     """
 
-    # TODO: Moved outside so not redefined on each call ?
-    terms = [
-        'http://rs.tdwg.org/dwc/terms/country',
-        'http://rs.tdwg.org/dwc/terms/decimalLatitude',
-        'http://rs.tdwg.org/dwc/terms/decimalLongitude',
-        'http://rs.tdwg.org/dwc/terms/kingdom',
-        'http://rs.tdwg.org/dwc/terms/locality',
-        'http://rs.tdwg.org/dwc/terms/Occurrence'
-    ]
-
-    return next(t for t in terms if t.endswith('/' + short))
+    return next(t for t in TERMS if t.endswith('/' + short))
