2d33463b649f06f026bd117922afec02973dfc7e
Nicolas Noé <nicolas@niconoe.org>
2013-05-28 13:41:23 +0200

    Basic distutils needed files.

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='python-dwca-reader',
+    version='0.1.0',
+    author=u'Nicolas Noé - Belgian Biodiversity Platform',
+    author_email='n.noe@biodiversity.be',
+    packages=['dwca', 'dwca.darwincore', 'dwca.test'],
+    url="https://github.com/BelgianBiodiversityPlatform/python-dwca-reader",
+    license='BSD licence, see LICENCE.txt',
+    description='A simple Python class to read Darwin Core Archive (DwC-A) files.',
+    long_description=open('README.rst').read(),
+    install_requires=["BeautifulSoup==3.2.1"]
+)
