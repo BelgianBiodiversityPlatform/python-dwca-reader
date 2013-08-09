89a1e6887c0b3b6c600fcab16a5b7c62302628f5
Nicolas Noé <nicolas@niconoe.org>
2013-08-09 09:38:39 +0200

    Updated package requirements to include XML parser (lxml).

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='python-dwca-reader',
-    version='0.3.0',
+    version='0.3.1',
     author=u'Nicolas Noé - Belgian Biodiversity Platform',
     author_email='n.noe@biodiversity.be',
     packages=['dwca', 'dwca.darwincore', 'dwca.test'],
@@ -11,5 +11,5 @@ setup(
     license='BSD licence, see LICENCE.txt',
     description='A simple Python class to read Darwin Core Archive (DwC-A) files.',
     long_description=open('README.rst').read(),
-    install_requires=["beautifulsoup4==4.2.1"]
+    install_requires=["beautifulsoup4==4.2.1", "lxml==3.2.3"]
 )
