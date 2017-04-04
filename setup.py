186c9fa9387c9df3301e035c319e50ce6c7c2363
Nicolas Noé <nicolas@niconoe.eu>
2017-04-04 18:18:16 +0200

    Prepare for release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='python-dwca-reader',
-    version='0.10.0',
+    version='0.10.1',
     author=u'Nicolas Noé - Belgian Biodiversity Platform',
     author_email='n.noe@biodiversity.be',
     packages=['dwca', 'dwca.darwincore', 'dwca.test'],
