00c6349efc8d454a7164f50cf865bed8a82b9cf9
Nicolas Noé <nicolas.noe@inbo.be>
2020-06-02 14:24:38 +0200

    Simplified Travis-CI configuration

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ setup(
         'typing;python_version<"3.5"',
     ],
     classifiers=[
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ]
 )
